bff20b4e7208ec2b858fa1e2274364af216b4011
sarthakn658-debug <sarthakn658@gmail.com>
2026-06-13 23:08:50 +0530

    update project files

---
+++ app/page.tsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import ExploreBtn from './components/ExploreBtn'
+import EventCard from './components/EventCard'
+import { events } from '../lib/constants'
+import { IEvent } from '@/database';
+import { cacheLife } from 'next/cache';
+
+ const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
+const Page =async () => {
+  "use cache";
+cacheLife("hours")
+const response = await fetch(`${BASE_URL}/api/events`);
+ const {events} = await response.json();
+  return (
+      <section>
+        <h1 className='text-center '>
+          The Hub for Energy Dev
+        <br/>
+          Event You Can't Miss
+        </h1>
+        <p className="text-center mt-5">Hackathons, Meetups, and Conference, All in one Place</p>
+        <ExploreBtn />
+       
+        <div className=' mt-20 space-y-7'> 
+        <h3>Feactured Events</h3>
+        <ul className='events list-none'>
+      {events && events.length > 0 && events.map((event: IEvent)=>( 
+        <li key={event.title}>
+          <EventCard{...event} />
+        </li>
+      ))}
+        </ul>
+        </div>
+     
+      </section>
+  
+  )
+}
+export default Page
