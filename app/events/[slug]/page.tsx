7e7f6b65e7a018796e73f08d17290dcda205ca42
Sarthak Negi <sarthakn658@gmail.com>
2026-06-27 21:12:20 +0530

    Refactor EventDetailsPage component structure

--- app/events/[slug]/page.tsx
+++ app/events/[slug]/page.tsx
@@ -1,6 +1,5 @@
 import {Suspense} from "react";
 import EventDetails from "@/app/components/EventDetails";
-
 const EventDetailsPage = async ({ params }: { params: Promise<{ slug: string }>}) => {
     const slug = params.then((p) => p.slug);
 
